06b08d3fbde6a9237f27e51606a4ca6c4915c817
Hafizah Osman <hafizahosman@nus.edu.sg>
2022-04-20 12:39:18 +0800

    ERC Web Tour
    ERC Web Tour created on 20 April 2022

---
+++ data.js
@@ -0,0 +1,218 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-utown-erc-foyer-",
+      "name": "UTown ERC Foyer ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.5922773022040682,
+        "pitch": 0.3918187924281824,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7137616916931115,
+          "pitch": 0.09921091130160598,
+          "rotation": 0,
+          "target": "1-utown-erc-open-space"
+        },
+        {
+          "yaw": -1.7747281645422532,
+          "pitch": 0.046149563094346036,
+          "rotation": 0,
+          "target": "2-utown-erc-study-area"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.89588631731936,
+          "pitch": 0.1716103757414622,
+          "title": "ERC Dropoff Point",
+          "text": "For drop-offs and pick-ups, this is the first thing you see when you get to the ERC."
+        },
+        {
+          "yaw": 0.00043403634413685666,
+          "pitch": -0.009927374765581476,
+          "title": "SRC",
+          "text": "The building across from here is the ERC. It houses a gym, a swimming pool, and food places. Linked to the ERC via the covered walkway to the right."
+        },
+        {
+          "yaw": -1.2348011083489876,
+          "pitch": -0.0014107547983286395,
+          "title": "Going Up",
+          "text": "This up-riding escalator will take you to level 2 where you will see seminar rooms, the SCALE office, and study areas for students.&nbsp;"
+        },
+        {
+          "yaw": -1.690007464568959,
+          "pitch": 0.04834067249323226,
+          "title": "Going Down",
+          "text": "Take the stairs just around this corner to get to Starbucks and open study spaces."
+        },
+        {
+          "yaw": -2.6244718484008,
+          "pitch": 0.09930393911983693,
+          "title": "Walking on Sunshine",
+          "text": "Head over to the open space for some fresh air and sunshine."
+        }
+      ]
+    },
+    {
+      "id": "1-utown-erc-open-space",
+      "name": "UTown ERC Open Space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.05907268237519503,
+        "pitch": -0.04945285729674609,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4109953575740413,
+          "pitch": 0.116206428948459,
+          "rotation": 0,
+          "target": "0-utown-erc-foyer-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.10902755767326155,
+          "pitch": 0.12459696234011552,
+          "title": "Tembusu Trees",
+          "text": "These tembusu trees are ubiquitous around UTown. These stand tall in the open space right in the centre of the ERC."
+        },
+        {
+          "yaw": 1.3229417090573712,
+          "pitch": 0.18234276746538036,
+          "title": "Take a load off",
+          "text": "Space to take a breather and enjoy the sights and sounds of the ERC."
+        },
+        {
+          "yaw": -2.8437974609572834,
+          "pitch": 0.13196239192003034,
+          "title": "Tembusu Trees",
+          "text": "These tembusu trees are ubiquitous around UTown. These stand tall in the open space right in the centre of the ERC."
+        },
+        {
+          "yaw": -2.3079546927784644,
+          "pitch": 0.11024115393484735,
+          "title": "To the foyer we go ...",
+          "text": "Head out to the foyer to get to the drop off point."
+        }
+      ]
+    },
+    {
+      "id": "2-utown-erc-study-area",
+      "name": "UTOWN ERC Study Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.9282514125003836,
+        "pitch": 0.41478981771351187,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.391200353727317,
+          "pitch": 0.04841039571875072,
+          "rotation": 0,
+          "target": "0-utown-erc-foyer-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.102247754457906,
+          "pitch": 0.10160972997683615,
+          "title": "Gimme my coffee",
+          "text": "Entrance to Starbucks, where they serve the staple beverage for all students.&nbsp;"
+        },
+        {
+          "yaw": 0.983057694618422,
+          "pitch": 0.07943462699747883,
+          "title": "Study Spaces",
+          "text": "This is just one of the many study spaces around the ERC. They get snapped up really fast, especially around exam period.&nbsp;"
+        },
+        {
+          "yaw": -2.3045355863299317,
+          "pitch": 0.05489860507990407,
+          "title": "Going Up",
+          "text": "Take the lift around the corner to get back up to level 1 of the ERC."
+        },
+        {
+          "yaw": -1.739662030645789,
+          "pitch": -0.1334854298617838,
+          "title": "Munchies, On The Go",
+          "text": "Quick snacks to fill the tummy while studying."
+        },
+        {
+          "yaw": -0.7008051722662785,
+          "pitch": -0.09147840807429297,
+          "title": "Book Drop",
+          "text": "Students don't need to go to the library to return their books. Such book drops are available across campus."
+        }
+      ]
+    }
+  ],
+  "name": "UTown ERC Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
